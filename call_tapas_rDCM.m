5c6ce0c722d35563fdb172d2b9769b898745b1b8
till-m <tmuser@ethz.ch>
2021-05-10 18:31:46 +0200

    Add CLI support, auto-concatenate for multiple files, manual matlab execution mode, fix bug in matlab, overall clean-up and documentation.

--- call_tapas_rDCM.m
+++ call_tapas_rDCM.m
@@ -6,28 +6,18 @@ rng(2406,'twister') % is this still necessary?
 P        = mfilename('fullpath');
 rDCM_ind = strfind(P,fullfile('rDCM','code'));
 
-% UNUSED UNUSED UNUSED
-% specify the options for the rDCM analysis
-options.SNR             = 3;
-options.y_dt            = 0.5;
-options.p0_all          = 0.15;  % single p0 value (for computational efficiency)
-options.iter            = 100;
-options.filter_str      = 5;
-options.restrictInputs  = 1;
-% UNUSED UNUSED UNUSED
 
 fprintf('Load data\n')
-DCM = load('.temp/in.mat');
+Y = load('.temp/in.mat');
 
-disp(DCM);
 % get time
 currentTimer = tic;
-DCM = tapas_rdcm_model_specification(DCM.Y, DCM.U, []);
+DCM = tapas_rdcm_model_specification(Y, [], []);
 
 disp(DCM)
 
 type = 'r';
-methods = 2;
+methods = 1;
 [output, options] = tapas_rdcm_estimate(DCM, type, methods=methods);
 
 % output elapsed time
@@ -38,13 +28,13 @@ toc(currentTimer)
 %% visualize the results
 
 % regions for which to plot traces
-plot_regions = [1 12];
+%plot_regions = [1 12];
 
 % visualize the results
-tapas_rdcm_visualize(output, DCM, options, plot_regions, 1)
+%tapas_rdcm_visualize(output, DCM, options, plot_regions, 1)
 
 
-
+%% Export results.
 fprintf('Save to .temp/')
 save('.temp/out.mat', 'output')
 
--- main.py
+++ main.py
@@ -1,61 +1,118 @@
+"""Expected function call:
+python main.py path/to/file01.nii.gz [path/to/file02.nii.gz ...] [--scheme (harvox | yeo | aal)] [--manual]
+"""
 import os
+import argparse
 import numpy as np
 import nibabel as nib
 from scipy.io import savemat, loadmat
-import TNM_project_input_data as pid
-import matlab.engine
+import data_utils as du
 
-matlab_engine = matlab.engine.start_matlab()
+parser = argparse.ArgumentParser()
+parser.add_argument('filepaths',
+                    help='Path(s) to .nii or .nii.gz file(s).',
+                    nargs='+')
+parser.add_argument('--scheme',
+                    default='yeo',
+                    help='Which parcellation scheme to use.')
+parser.add_argument('--manual',
+                    dest='manual',
+                    action='store_true',
+                    help='Run matlab code manually.')
+parser.set_defaults(manual=False)
+
+args = parser.parse_args()
+
+FILEPATHS = args.filepaths  #.split()
+SCHEME = args.scheme
+MANUAL_MATLAB = args.manual
+
+try:
+    import matlab.engine
+    matlab_engine = matlab.engine.start_matlab()
+except ImportError:
+    print(
+        "\n\n\nMatlab Engine API not found. Please execute the matlab script manually when prompted\n\n\n"
+    )
+    MANUAL_MATLAB = True
 
 
 def call_tapas_rDCM(header, time_series):
+    '''
+    Parameters
+    ----------
+    header : nibabel.nifti1.Nifti1Header
+        Nifti1 image header.
+    time_series : numpy.ndarray
+        2-dimensional array of shape (time_steps, regions) containg BOLD signal
+        data.
+
+    Returns
+    -------
+    rDCK: some weird type #TODO: Figure this out.
+        rDCM result.
+
+    '''
     try:
         os.mkdir('.temp')
     except FileExistsError:
         pass
 
+    # TODO: Make use of region names.
     to_mat = {
-        'a': [],
-        'b': [],
-        'c': [],
-        'd': [],
-        'Y': {
-            'y': freq_series,
-            'dt': 0.5
-            # header.get_zooms()[-1]  # last value is time between scans in ms
-        },
-        'U': {
-            'u': np.zeros((16 * freq_series.shape[0], 1)),
-            # 'dt': header.get_zooms()[-1] / 16
-        },
-        'v': freq_series.shape[0],  # specify number of datapoints (per region)
-        'n': freq_series.shape[1]  # specify number of regions
+        'y': time_series,
+        'dt': 0.5
+        # header.get_zooms()[-1]  # last value is time between scans in ms
     }
-
-    print(to_mat['Y']['y'].shape)
-    print(to_mat['U']['u'].shape)
 
     savemat('.temp/in.mat', to_mat)
 
-    # call matlab script
-    matlab_engine.call_tapas_rDCM(nargout=0)
+    # memory management to prevent me from dropping out of calls while running
+    # the script in the future:
+    del to_mat
+
+    if MANUAL_MATLAB:
+        print("Please execute the matlab script now.")
+        print("After successful execution, press Enter to continue...")
+        input()
+    else:
+        # call matlab script
+        matlab_engine.call_tapas_rDCM(nargout=0)
 
     rDCM = loadmat('.temp/out.mat')
-    # add cleanup such that .temp/ is deleted?
+    print("\nSuccesfully loaded rDCM output.")
+    # TODO: figure out whether to delete .temp/
     return rDCM
 
 
-def rDCM_from_fMRI(path):
-    img = nib.load(path)
-    parcellated_img = pid.parcellation("Yeo", img)
-    return call_tapas_rDCM(img.header, parcellated_img[0])
+def rDCM_from_fMRI(filepaths):
+    '''
+    Parameters
+    ----------
+    filepaths : list
+        List of paths pointing to .nii or .nii.gz files to use for rDCM.
 
+    Returns
+    -------
+    rDCK: some weird type #TODO: Figure this out.
+        rDCM result.
 
-def example_call():
-    rDCM_from_fMRI(
-        'data/ds003059/sub-001/ses-LSD/func/sub-001_ses-LSD_task-rest_run-01_bold.nii.gz'
-    )
+    '''
+    images = []
+    for path in filepaths:
+        images.append(nib.load(path))
+
+    header0 = images[0].header
+
+    # TODO: Make use of region names.
+    time_series = du.parcellation(SCHEME, images[0])[0]
+
+    for img in images[1:]:
+        pi = du.parcellation(SCHEME, img)[0]
+        time_series = du.combine_series(time_series, pi)
+
+    return call_tapas_rDCM(header0, time_series)
 
 
 if __name__ == '__main__':
-    example_call()
+    rDCM_from_fMRI(FILEPATHS)
